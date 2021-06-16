d34f9d89afa616ca80c77f7d6699f2f97f9ef496
Usama Rao <usamarao308@gmail.com>
2021-06-16 09:48:49 +0500

    Interface of Quiz game is done and some backend functionality

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public Question[] questions;
+    private static List<Question> unAnsQes;
+
+    private Question currentQuestion;
+
+    void Start()
+    {
+        if(unAnsQes== null || unAnsQes.Count==0)
+        {
+            unAnsQes=questions.ToList<Question>();
+        }
+
+        GetRandomQuestion();
+
+        Debug.Log(currentQuestion.facts + " is "+ currentQuestion.isTrue);
+        
+    }
+
+    void GetRandomQuestion()
+    {
+        int randQesIndex= Random.Range(0, unAnsQes.Count);
+        currentQuestion=unAnsQes[randQesIndex];
+
+        unAnsQes.RemoveAt(randQesIndex);
+
+
+    }
+}
---
+++ Assets/Question.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Question 
+{
+    public string facts;
+    public bool isTrue;
+
+
+}
